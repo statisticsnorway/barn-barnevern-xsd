47a8b0a236c1204d97d31a0d2e2be0393787de94
Roar Skinderviken <roar.skinderviken@gmail.com>
2021-11-17 10:41:15 +0100

    Converted main library class to Kotlin

---
+++ src/main/kotlin/no/ssb/barn/TheValidator.kt
@@ -0,0 +1,35 @@
+package no.ssb.barn
+
+/**
+ * Barn XML validator. Clients of this library will typically keep an instance
+ * of this class as a bean.
+ *
+ * @author roar.skinderviken@ssb.no
+ * @since 1.0
+ */
+class TheValidator private constructor() {
+
+    /**
+     * Validates XML and returns validation report on JSON format.
+     *
+     * @param xsdVersion XSD version for xmlBody
+     * @param xmlBody    XML body
+     * @return Validation report on JSON format
+     */
+    fun validate(xsdVersion: Int, xmlBody: String): String {
+        return "{hello: \"world\"}"
+    }
+
+    companion object {
+        /**
+         * Builds a validator instance.
+         *
+         * @return [TheValidator] instance
+         */
+        fun create(): TheValidator {
+
+            // assemble the validator here
+            return TheValidator()
+        }
+    }
+}
