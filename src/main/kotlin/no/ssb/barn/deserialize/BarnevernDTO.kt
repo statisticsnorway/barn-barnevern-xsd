e59c8699999dd168857269089d534ecaf90a1754
Hyemi <kons_kim@ssb.no>
2021-11-17 16:02:58 +0100

    1st versjon av deserialisering

---
+++ src/main/kotlin/no/ssb/barn/deserialize/BarnevernDTO.kt
@@ -0,0 +1,17 @@
+package no.ssb.barn.deserialize
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import no.ssb.barn.xsd.*
+import java.util.*
+
+@JsonSerialize
+@JsonDeserialize
+data class BarnevernDTO (
+        val userName: String,
+        val userId: String?,
+        val datouttrekk: Date?,
+        val avgiver: AvgiverType?,
+        val sak: SakType?
+)
+
