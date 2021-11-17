bf9300941a3418ddf041ef6dcffb1bb3d7a89668
Roar Skinderviken <roar.skinderviken@gmail.com>
2021-11-17 21:46:36 +0100

    Added test

--- src/test/groovy/no/ssb/barn/validation2/TheValidatorSpec.groovy
+++ src/test/groovy/no/ssb/barn/validation2/TheValidatorSpec.groovy
@@ -9,7 +9,7 @@ class TheValidatorSpec extends Specification {
         TheValidator.create() instanceof TheValidator
     }
 
-    def "when validate with valid params, receive JSON result"() {
+    def "when validate with invalid params, receive JSON result"() {
         given:
         def sut = TheValidator.create()
 
@@ -20,5 +20,25 @@ class TheValidatorSpec extends Specification {
         result.startsWith("{\"journalId\"")
         and:
         result.contains("warningLevel\":\"FATAL\"")
+    }
+
+    def "when validate with valid params, receive JSON result"() {
+        given:
+        def sut = TheValidator.create()
+
+        when:
+        def result = sut.validate(
+                1,
+                getResourceAsText("test01_fil01.xml"))
+
+        then:
+        result.startsWith("{\"journalId\"")
+        and:
+        !result.contains("warningLevel")
+    }
+
+    def getResourceAsText(String resourceName) {
+        return new String(getClass().getClassLoader()
+                .getResourceAsStream(resourceName).readAllBytes())
     }
 }
