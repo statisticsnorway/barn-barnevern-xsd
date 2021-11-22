b342cde226ca9c3730d284034d4e2004cb637639
Hyemi <kons_kim@ssb.no>
2021-11-22 15:23:10 +0100

    3rd versjon av marshalling

---
+++ src/test/groovy/no/ssb/barn/deserialize/DataConvertXSpec.groovy
@@ -0,0 +1,41 @@
+package no.ssb.barn.deserialize
+
+import no.ssb.barn.xsd.BarnevernType
+import spock.lang.Specification
+
+import javax.xml.transform.Source
+import javax.xml.transform.stream.StreamSource
+
+class DataConvertXSpec extends Specification {
+
+    def "skriv ut data object fra xsd"() {
+        given:
+            Source sourceXSD = getSourceFromClasspath("/Barnevern.xsd")
+
+        when:
+            BarnevernType convertObj = DataConvertX.unmarshallXML(sourceXSD)
+
+        then:
+            convertObj.toString()
+
+    }
+
+    def "skriv ut data object as schema object"() {
+        given:
+        def sourceXSD = File.createTempFile("Barnevern", ".xsd")
+
+        when:
+        def convertObj = DataConvertX.unmarshallXMLwithSchema(sourceXSD)
+
+        then:
+        convertObj.toString()
+    }
+
+    def getSourceFromClasspath(String path) {
+        try {
+            return new StreamSource(getClass().getResourceAsStream(path))
+        } catch (Exception ignored) {
+            return null
+        }
+    }
+}
