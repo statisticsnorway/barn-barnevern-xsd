c2dd4f3041f0db99691c1c04fe8e390243c14ee4
Roar Skinderviken <roar.skinderviken@gmail.com>
2021-11-24 21:33:40 +0100

    Added 10 validation rules to main validator

--- src/main/kotlin/no/ssb/barn/validation/VersionOneValidator.kt
+++ src/main/kotlin/no/ssb/barn/validation/VersionOneValidator.kt
@@ -1,5 +1,6 @@
 package no.ssb.barn.validation
 
+import no.ssb.barn.deserialize.BarnevernDeserializer
 import no.ssb.barn.framework.ValidationContext
 import no.ssb.barn.framework.ValidatorContract
 import no.ssb.barn.report.ValidationReport
@@ -33,8 +34,14 @@ class VersionOneValidator : ValidatorContract {
                 .flatten()
                 .toList()
                 .ifEmpty {
+                    // this is not a great solution, fix me
+                    val innerContext = ValidationContext(
+                        context.xml,
+                        BarnevernDeserializer.unmarshallXml(context.xml)
+                    )
+
                     rules.asSequence()
-                        .mapNotNull { it.validate(context) }
+                        .mapNotNull { it.validate(innerContext) }
                         .flatten()
                         .toList()
                 }
