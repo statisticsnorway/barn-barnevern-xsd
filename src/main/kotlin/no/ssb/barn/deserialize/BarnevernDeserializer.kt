9557863f25aa89a7727661985f535b7d817b89e5
Roar Skinderviken <roar.skinderviken@gmail.com>
2021-11-25 19:49:48 +0100

    Removed unused code

--- src/main/kotlin/no/ssb/barn/deserialize/BarnevernDeserializer.kt
+++ src/main/kotlin/no/ssb/barn/deserialize/BarnevernDeserializer.kt
@@ -1,42 +1,15 @@
 package no.ssb.barn.deserialize
 
 import jakarta.xml.bind.JAXBContext
-import jakarta.xml.bind.Unmarshaller
 import no.ssb.barn.xsd.BarnevernType
-import java.io.InputStream
-import javax.xml.XMLConstants
-import javax.xml.transform.stream.StreamSource
-import javax.xml.validation.SchemaFactory
 
 
 class BarnevernDeserializer {
-
     companion object {
-
         @JvmStatic
         fun unmarshallXml(xml: String): BarnevernType =
-            unmarshallXml(xml.byteInputStream())
-
-        @JvmStatic
-        fun unmarshallXml(xmlStream: InputStream): BarnevernType =
             JAXBContext.newInstance(BarnevernType::class.java)
                 .createUnmarshaller()
-                .unmarshal(xmlStream) as BarnevernType
-
-        @JvmStatic
-        fun unmarshallXmlWithSchema(
-            schemaStream: InputStream,
-            xmlStream: InputStream
-        ): BarnevernType? {
-            val unmarshaller: Unmarshaller = JAXBContext
-                .newInstance(BarnevernType::class.java)
-                .createUnmarshaller()
-
-            unmarshaller.schema =
-                SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
-                    .newSchema(StreamSource(schemaStream))
-
-            return unmarshaller.unmarshal(xmlStream) as BarnevernType?
-        }
+                .unmarshal(xml.byteInputStream()) as BarnevernType
     }
 }
--- src/test/groovy/no/ssb/barn/deserialize/BarnevernDeserializerSpec.groovy
+++ src/test/groovy/no/ssb/barn/deserialize/BarnevernDeserializerSpec.groovy
@@ -9,31 +9,7 @@ class BarnevernDeserializerSpec extends Specification {
     @Unroll("test01_fil0 #i .xml")
     def "when unmarshalling valid XML, receive populated instance"() {
         given:
-        def xmlStream = TestDataProvider.getResourceAsStream("test01_fil0" + i + ".xml")
-
-        when:
-        def barnevernType = BarnevernDeserializer.unmarshallXml(xmlStream)
-
-        then:
-        noExceptionThrown()
-        and:
-        null != barnevernType
-        and:
-        null != barnevernType.avgiver
-        and:
-        null != barnevernType.datoUttrekk
-        and:
-        null != barnevernType.fagsystem
-        and:
-        null != barnevernType.sak
-
-        where:
-        i << (1..9)
-    }
-
-    def "when unmarshalling valid XML as String, receive populated instance"() {
-        given:
-        def xml = TestDataProvider.getResourceAsString("test01_fil01.xml")
+        def xml = TestDataProvider.getResourceAsString("test01_fil0" + i + ".xml")
 
         when:
         def barnevernType = BarnevernDeserializer.unmarshallXml(xml)
@@ -50,28 +26,8 @@ class BarnevernDeserializerSpec extends Specification {
         null != barnevernType.fagsystem
         and:
         null != barnevernType.sak
-    }
 
-    def "when unmarshalling valid XML with XSD, receive populated instance"() {
-        given:
-        def xsdStream = TestDataProvider.getResourceAsStream("Barnevern.xsd")
-        and:
-        def xmlStream = TestDataProvider.getResourceAsStream("test01_fil01.xml")
-
-        when:
-        def barnevernType = BarnevernDeserializer.unmarshallXmlWithSchema(xsdStream, xmlStream)
-
-        then:
-        noExceptionThrown()
-        and:
-        null != barnevernType
-        and:
-        null != barnevernType.avgiver
-        and:
-        null != barnevernType.datoUttrekk
-        and:
-        null != barnevernType.fagsystem
-        and:
-        null != barnevernType.sak
+        where:
+        i << (1..9)
     }
 }
