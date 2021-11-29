e04b156d2c269a4f366cc7a5a1ba4b4a7aeb4eea
Roar Skinderviken <roar.skinderviken@gmail.com>
2021-11-29 18:30:05 +0100

    BAR-122: Added JonOleTodo's

--- src/main/kotlin/no/ssb/barn/validation/rule/JonOleTodoInvestigationEndDateRequired.kt
+++ src/main/kotlin/no/ssb/barn/validation/rule/JonOleTodoInvestigationEndDateRequired.kt
@@ -6,7 +6,7 @@ import no.ssb.barn.report.ReportEntry
 import no.ssb.barn.report.WarningLevel
 import no.ssb.barn.xsd.UndersokelseType
 
-class TodoInvestigationEndDateRequired : AbstractRule(
+class JonOleTodoInvestigationEndDateRequired : AbstractRule(
     WarningLevel.ERROR,
     "Undersøkelse Kontroll 2d: Avslutta 31 12 medfører at sluttdato skal være satt på undersøkelsen",
     UndersokelseType::class.java.simpleName
