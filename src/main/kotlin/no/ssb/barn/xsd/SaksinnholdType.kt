0b29df408f4fddc663e91fc3cc84b0633acabd28
Roar Skinderviken <roar.skinderviken@gmail.com>
2021-12-01 13:54:49 +0100

    BAR-122: Fixed code duplication #6

--- src/main/kotlin/no/ssb/barn/xsd/SaksinnholdType.kt
+++ src/main/kotlin/no/ssb/barn/xsd/SaksinnholdType.kt
@@ -25,86 +25,36 @@ data class SaksinnholdType(
         private val validFrom2020 = LocalDate.of(2020, 1, 1)
         private val codeList =
             listOf(
-                CodeListItem("1",
-                    "Foreldres somatiske sykdom", validFrom2013),
-                CodeListItem("2",
-                    "Foreldres psykiske problem/ lidelse",
-                    validFrom2013
-                ),
-                CodeListItem("3",
-                    "Foreldres rusmisbruk",
-                    validFrom2013
-                ),
+                CodeListItem("1", "Foreldres somatiske sykdom", validFrom2013),
+                CodeListItem("2", "Foreldres psykiske problem/ lidelse", validFrom2013),
+                CodeListItem("3", "Foreldres rusmisbruk", validFrom2013),
                 CodeListItem("4",
                     "Foreldres manglende foreldreferdigheter",
                     validFrom2013,
                     LocalDate.of(2019, 12, 31),
                     "4 deles i 20, 21, 22, 23 og 24"
                 ),
-                CodeListItem("20",
-                    "Foreldres manglende beskyttelse av barnet",
-                    validFrom2020
-                ),
-                CodeListItem("21",
-                    "Foreldres manglende stimulering og regulering av barnet",
-                    validFrom2020
-                ),
-                CodeListItem("22",
-                    "Foreldres manglende sensitivitet og følelsesmessige tilgjengelighet for barnet",
-                    validFrom2020
-                ),
+                CodeListItem("20", "Foreldres manglende beskyttelse av barnet", validFrom2020),
+                CodeListItem("21", "Foreldres manglende stimulering og regulering av barnet", validFrom2020),
+                CodeListItem("22", "Foreldres manglende sensitivitet og følelsesmessige tilgjengelighet for barnet", validFrom2020),
                 CodeListItem("23",
                     "Foreldres manglende oppfølging av barnets behov for barnehage, skole og pedagogiske tjenester",
-                    validFrom2020
-                ),
-                CodeListItem("24",
-                    "Konflikt mellom foreldre som ikke bor sammen",
-                    validFrom2020
-                ),
-                CodeListItem("5",
-                    "Foreldres kriminalitet",
-                    validFrom2013
-                ),
-                CodeListItem("6",
-                    "Høy grad av konflikt hjemme",
-                    validFrom2013
-                ),
-                CodeListItem("7",
-                    "Vold i hjemmet/ barnet vitne til vold i nære relasjoner ",
-                    validFrom2013
-                ),
-                CodeListItem("8",
+                    validFrom2020),
+                CodeListItem("24", "Konflikt mellom foreldre som ikke bor sammen", validFrom2020),
+                CodeListItem("5", "Foreldres kriminalitet", validFrom2013),
+                CodeListItem("6", "Høy grad av konflikt hjemme", validFrom2013),
+                CodeListItem("7", "Vold i hjemmet/ barnet vitne til vold i nære relasjoner ", validFrom2013),
+                CodeListItem(
+                    "8",
                     "Barnet utsatt for vanskjøtsel (Barnet overlatt til seg selv, dårlig kosthold, dårlig hygiene)",
-                    validFrom2013
-                ),
-                CodeListItem("9",
-                    "Barnet utsatt for fysisk vold",
-                    validFrom2013
-                ),
-                CodeListItem("10",
-                    "Barnet utsatt for psykisk vold",
-                    validFrom2013
-                ),
-                CodeListItem("11",
-                    "Barnet utsatt for seksuelle overgrep",
-                    validFrom2013
-                ),
-                CodeListItem("12",
-                    "Barnet mangler omsorgsperson",
-                    validFrom2013
-                ),
-                CodeListItem("13",
-                    "Barnet har nedsatt funksjonsevne",
-                    validFrom2013
-                ),
-                CodeListItem("14",
-                    "Barnets psykiske problem/lidelse",
-                    validFrom2013
-                ),
-                CodeListItem("15",
-                    "Barnets rusmisbruk",
-                    validFrom2013
-                ),
+                    validFrom2013),
+                CodeListItem("9", "Barnet utsatt for fysisk vold", validFrom2013),
+                CodeListItem("10", "Barnet utsatt for psykisk vold", validFrom2013),
+                CodeListItem("11", "Barnet utsatt for seksuelle overgrep", validFrom2013),
+                CodeListItem("12", "Barnet mangler omsorgsperson", validFrom2013),
+                CodeListItem("13", "Barnet har nedsatt funksjonsevne", validFrom2013),
+                CodeListItem("14", "Barnets psykiske problem/lidelse", validFrom2013),
+                CodeListItem("15", "Barnets rusmisbruk", validFrom2013),
                 CodeListItem("16",
                     "Barnets atferd/ kriminalitet",
                     validFrom2013,
@@ -115,18 +65,9 @@ data class SaksinnholdType(
                     "Barnets relasjonsvansker(mistanke om eller diagnostiserte tilknytningsvansker, problematikk knyttet til samspillet mellom barn og omsorgspersoner)",
                     validFrom2013
                 ),
-                CodeListItem("25",
-                    "Barnets atferd",
-                    validFrom2020
-                ),
-                CodeListItem("26",
-                    "Barnets kriminelle handlinger",
-                    validFrom2020
-                ),
-                CodeListItem("27",
-                    "Barnet utsatt for menneskehandel",
-                    validFrom2020
-                ),
+                CodeListItem("25", "Barnets atferd", validFrom2020),
+                CodeListItem("26", "Barnets kriminelle handlinger", validFrom2020),
+                CodeListItem("27", "Barnet utsatt for menneskehandel", validFrom2020),
                 CodeListItem("18",
                     "Andre forhold ved foreldre/ familien (krever presisering)(Denne kategorien skal kunne benyttes dersom ingen av kategoriene 1-27 passer.)",
                     validFrom2013
