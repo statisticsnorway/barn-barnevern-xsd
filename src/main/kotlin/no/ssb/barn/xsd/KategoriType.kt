00fa66193e85ab8b6f33910b096985ec2e581428
Roar Skinderviken <roar.skinderviken@gmail.com>
2021-12-01 13:57:58 +0100

    BAR-122: Fixed code duplication #7

--- src/main/kotlin/no/ssb/barn/xsd/KategoriType.kt
+++ src/main/kotlin/no/ssb/barn/xsd/KategoriType.kt
@@ -42,13 +42,10 @@ data class KategoriType(
                 Pair("3.4", "Weber Stratton"),
                 Pair("3.5", "ICDP"),
                 Pair("3.6", "Marte Meo"),
-                Pair(
-                    "3.7",
-                    "Andre hjemmebaserte tiltak <br>" +
+                Pair("3.7", "Andre hjemmebaserte tiltak <br>" +
                             "(gjelder andre tiltak i hjemmet som følger en spesiell <br>" +
                             "metodikk og som gjenomføres på en systematisk måte) <br>" +
-                            "(krever presisering)"
-                ),
+                            "(krever presisering)"),
                 Pair("3.8", "Sentre for foreldre og barn"),
                 Pair("3.9", "Vedtak om råd og veiledning"),
                 Pair("3.10", "Hjemmekonsulent/miljøarbeider"),
@@ -56,60 +53,37 @@ data class KategoriType(
                 Pair("4.1", "Barnehage"),
                 Pair("4.2", "SFO/ Aktivitetsskole"),
                 Pair("4.3", "Fritidsaktiviteter"),
-                Pair(
-                    "4.4",
-                    "Økonomisk hjelp for øvrig <br>" +
-                            "(gjelder økonomisk hjelp som ikke inngår i 4.1, 4.2 og 4.3)"
-                ),
+                Pair("4.4", "Økonomisk hjelp for øvrig <br>(gjelder økonomisk hjelp som ikke inngår i 4.1, 4.2 og 4.3)"),
                 Pair("4.5", "Besøkshjem/ avlastningstiltak"),
                 Pair("4.6", "Støttekontakt"),
-                Pair(
-                    "4.7",
-                    "Samtalegrupper/ barnegrupper <br>" +
-                            "(gjelder strukturerte samtale- og aktivitetstilbud for aldersbestemte grupper)."
-                ),
-                Pair(
-                    "4.8",
-                    "Utdanning og arbeid <br>" +
-                            "(gjelder oppfølging og støtte for å styrke barn og <br>" +
-                            "ungdoms tilknytning til skole eller arbeid)"
-                ),
+                Pair("4.7", "Samtalegrupper/ barnegrupper <br>"
+                        + "(gjelder strukturerte samtale- og aktivitetstilbud for aldersbestemte grupper)."),
+                Pair("4.8", "Utdanning og arbeid <br>"
+                            + "(gjelder oppfølging og støtte for å styrke barn og <br>"
+                            + "ungdoms tilknytning til skole eller arbeid)"),
                 Pair("4.9", "ART"),
                 Pair("4.99", otherMeasures),
-                Pair(
-                    "5.1",
-                    "Frivillig tilsyn i hjemmet <br>" +
-                            "(gjelder der foreldre har samtykket til tilsyn)"
-                ),
-                Pair(
-                    "5.2",
-                    "Pålagt tilsyn i hjemmet <br>" +
-                            "(gjelder der tilsyn skjer etter vedtak i fylkesnemnd)"
-                ),
+                Pair("5.1", "Frivillig tilsyn i hjemmet <br>"
+                            + "(gjelder der foreldre har samtykket til tilsyn)"),
+                Pair("5.2", "Pålagt tilsyn i hjemmet <br>(gjelder der tilsyn skjer etter vedtak i fylkesnemnd)"),
                 Pair("5.3", "Tilsyn under samvær"),
                 Pair("5.4", "Ruskontroll"),
                 Pair("5.99", otherMeasures),
                 Pair("6.1", "Familieråd"),
-                Pair(
-                    "6.2",
-                    "Nettverksmøter <br>" +
+                Pair("6.2", "Nettverksmøter <br>" +
                             "(Strukturerte dialogmøter som består av <br>" +
                             "barneverntjenesten, foreldre og ungdom og andre personer <br>" +
                             "som er viktige i ungdommens liv. Invitasjon av deltakere <br>" +
-                            "skjer i samråd mellom barneverntjenesten, ungdom og foreldre)"
-                ),
+                            "skjer i samråd mellom barneverntjenesten, ungdom og foreldre)"),
                 Pair("6.3", "Individuell plan"),
                 Pair("6.4", "Deltakelse i ansvarsgruppe"),
                 Pair("6.5", "Samarbeid om utdanning og arbeid"),
                 Pair("6.99", otherMeasures),
                 Pair("7.1", "Bvl § 4-10 medisinsk undersøkelse og behandling"),
                 Pair("7.2", "Bvl § 4-11 behandling av barn med særlige opplæringsbehov"),
-                Pair(
-                    "7.3",
-                    "Psykisk helsehjelp for barn og unge <br>" +
+                Pair("7.3", "Psykisk helsehjelp for barn og unge <br>" +
                             "(Gjelder ikke ved henvisninger, men når barn får behandling <br>" +
-                            "av psykolog eller innen psykisk helsevern/BUP)"
-                ),
+                            "av psykolog eller innen psykisk helsevern/BUP)"),
                 Pair("7.99", otherMeasures),
                 Pair("8.1", "Økonomisk hjelp ved etablering i egen bolig/ hybel (Ikke husleie)"),
                 Pair("8.2", "Bolig med oppfølging (inkluderer også bofellesskap)"),
