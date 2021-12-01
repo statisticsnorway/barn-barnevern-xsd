5a9286e2f26f03891852b30fb22da5024c472604
Roar Skinderviken <roar.skinderviken@gmail.com>
2021-12-01 13:39:42 +0100

    BAR-122: Fixed code duplication #3

--- src/main/kotlin/no/ssb/barn/xsd/MelderType.kt
+++ src/main/kotlin/no/ssb/barn/xsd/MelderType.kt
@@ -24,87 +24,28 @@ data class MelderType(
         private val validFrom: LocalDate = LocalDate.parse("2013-01-01")
 
         private val codeList = arrayOf(
-            arrayOf(
-                "1",
-                "Barnet selv"
-            ),
-            arrayOf(
-                "2",
-                "Mor/ far/ foresatte"
-            ),
-            arrayOf(
-                "3",
-                "Familie for øvrig"
-            ),
-            arrayOf(
-                "4",
-                "Andre privatpersoner"
-            ),
-            arrayOf(
-                "5",
-                "Barnvernstjenesten"
-            ),
-            arrayOf(
-                "6",
-                "NAV (kommune og stat)"
-            ),
-            arrayOf(
-                "7",
-                "Barnevernsvakt"
-            ),
-            arrayOf(
-                "8",
-                "Politi"
-            ),
-            arrayOf(
-                "9",
-                "Barnehage"
-            ),
-            arrayOf(
-                "10",
-                "Helsestasjon/skolehelsetjenesten"
-            ),
-            arrayOf(
-                "11",
-                "Skole"
-            ),
-            arrayOf(
-                "12",
-                "Pedagogisk-psykologisk tjeneste (PPT)"
-            ),
-            arrayOf(
-                "13",
-                "Psykisk helsevern for barn og unge (kommune og stat)"
-            ),
-            arrayOf(
-                "14",
-                "Psykisk helsevern for voksne (kommune og stat)"
-            ),
-            arrayOf(
-                "15",
-                "Lege/ sykehus/ tannlege"
-            ),
-            arrayOf(
-                "16",
-                "Familievernkontor"
-            ),
-            arrayOf(
-                "17",
-                "Tjenester og instanser med ansvar for oppfølging av personers rusproblemer (kommune og stat)"
-            ),
+            arrayOf("1", "Barnet selv"),
+            arrayOf("2", "Mor/ far/ foresatte"),
+            arrayOf("3", "Familie for øvrig"),
+            arrayOf("4", "Andre privatpersoner"),
+            arrayOf("5", "Barnvernstjenesten"),
+            arrayOf("6", "NAV (kommune og stat)"),
+            arrayOf("7", "Barnevernsvakt"),
+            arrayOf("8", "Politi"),
+            arrayOf("9", "Barnehage"),
+            arrayOf("10", "Helsestasjon/skolehelsetjenesten"),
+            arrayOf("11", "Skole"),
+            arrayOf("12", "Pedagogisk-psykologisk tjeneste (PPT)"),
+            arrayOf("13", "Psykisk helsevern for barn og unge (kommune og stat)"),
+            arrayOf("14", "Psykisk helsevern for voksne (kommune og stat)"),
+            arrayOf("15", "Lege/ sykehus/ tannlege"),
+            arrayOf("16", "Familievernkontor"),
+            arrayOf("17", "Tjenester og instanser med ansvar for oppfølging av personers rusproblemer (kommune og stat)"),
             arrayOf("18", "Krisesenter"),
             arrayOf("19", "Asylmottak/ UDI/ innvandringsmyndighet"),
-            arrayOf(
-                "20", "Utekontakt/ fritidsklubb"
-            ),
-            arrayOf(
-                "21",
-                "Frivillige organisasjoner/ idrettslag"
-            ),
-            arrayOf(
-                "22",
-                "Andre offentlige instanser (krever presisering)"
-            ),
+            arrayOf("20", "Utekontakt/ fritidsklubb"),
+            arrayOf("21", "Frivillige organisasjoner/ idrettslag"),
+            arrayOf("22", "Andre offentlige instanser (krever presisering)"),
             arrayOf("23", "Anonym")
         )
             .map { CodeListItem(it[0], it[1], validFrom) }
