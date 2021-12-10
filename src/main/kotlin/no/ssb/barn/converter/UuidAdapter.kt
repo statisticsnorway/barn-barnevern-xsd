cd26014f43d6c79eabd6bd3f73395dd8b61d96f5
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2021-12-10 03:59:06 +0100

    Added adapter for String to UUID conversion

--- src/main/kotlin/no/ssb/barn/converter/UuidAdapter.kt
+++ src/main/kotlin/no/ssb/barn/converter/UuidAdapter.kt
@@ -1,4 +1,14 @@
 package no.ssb.barn.converter
 
-class UuidAdapter {
+import jakarta.xml.bind.annotation.adapters.XmlAdapter
+import java.util.*
+
+class UuidAdapter: XmlAdapter<String, UUID>() {
+    override fun marshal(uuid: UUID): String {
+        return uuid.toString()
+    }
+
+    override fun unmarshal(string: String): UUID {
+        return UUID.fromString(string)
+    }
 }
