577f03341379c4b9c41c7b93a1c7967e9e61a87a
Roar Skinderviken <roar.skinderviken@gmail.com>
2021-12-14 01:34:26 +0100

    Small refactoring and added duplicate comment

--- src/main/kotlin/no/ssb/barn/converter/XMLConverter.kt
+++ src/main/kotlin/no/ssb/barn/converter/XMLConverter.kt
@@ -1,24 +1,25 @@
 package no.ssb.barn.converter
 
-import javax.xml.bind.JAXB
 import no.ssb.barn.xsd.BarnevernType
 import java.io.StringReader
 import java.io.StringWriter
+import javax.xml.bind.JAXB
 
 class XMLConverter {
     companion object {
+
+        // NOTE: Duplicate of BarnevernDeserializer
 
         @JvmStatic
         fun xmlToBarnevernType(xmlString: String): BarnevernType =
              JAXB.unmarshal(StringReader(xmlString), BarnevernType::class.java)
 
         @JvmStatic
-        fun barnevernTypeToXml(barnevernType: BarnevernType): String {
-            val stringWriter = StringWriter()
-            JAXB.marshal(barnevernType, stringWriter)
-
-            return stringWriter.toString()
-        }
+        fun barnevernTypeToXml(barnevernType: BarnevernType): String =
+            StringWriter().use {
+                JAXB.marshal(barnevernType, it)
+                return it.toString()
+            }
     }
 }
 
