6ab1e1cfbc2434fefc6ddea7d123b67ea8883093
Roar Skinderviken <roar.skinderviken@gmail.com>
2021-12-16 13:40:33 +0100

    Refactored InitialMutationProvider to functional style

--- src/main/kotlin/no/ssb/barn/generator/InitialMutationProvider.kt
+++ src/main/kotlin/no/ssb/barn/generator/InitialMutationProvider.kt
@@ -13,63 +13,61 @@ class InitialMutationProvider(xmlResourceName: String) {
     private val initialMutationXml: String = getResourceAsText(xmlResourceName)
 
     fun createInitialMutation(currentDate: LocalDate): BarnevernType {
-        val instance = BarnevernConverter.unmarshallXml(initialMutationXml)
         val currentDateTime =
             currentDate.atStartOfDay().plusHours((6..20).random().toLong())
-        var globalCompanyId: String
 
-        with(instance) {
-            datoUttrekk = currentDateTime
-            fagsystem = RandomUtils.generateRandomFagsystemType()
-            avgiver = RandomUtils.generateRandomAvgiverType()
-            globalCompanyId = avgiver.organisasjonsnummer
-        }
-
-        with(instance.sak) {
-            startDato = currentDateTime.toLocalDate()
-            id = java.util.UUID.randomUUID()
-            journalnummer =
-                RandomUtils.generateRandomString(15)
-            fodselsnummer = RandomUtils.generateRandomSSN(
-                LocalDate.now().minusYears(20),
-                LocalDate.now().minusYears(1)
-            )
-        }
-
-        if (!instance.sak.virksomhet.any()) {
-            return instance
-        }
-
-        with(instance.sak.virksomhet.first()) {
-            startDato = currentDateTime.toLocalDate()
-            organisasjonsnummer = globalCompanyId // TODO: Oslo
-
-            if (!melding.any()) {
-                return instance
+        return BarnevernConverter.unmarshallXml(initialMutationXml)
+            .apply {
+                with(sak) {
+                    startDato = currentDateTime.toLocalDate()
+                    id = java.util.UUID.randomUUID()
+                    journalnummer =
+                        RandomUtils.generateRandomString(15)
+                    fodselsnummer = RandomUtils.generateRandomSSN(
+                        LocalDate.now().minusYears(20),
+                        LocalDate.now().minusYears(1)
+                    )
+                }
             }
+            .also {
+                if (!it.sak.virksomhet.any()) {
+                    return it
+                }
+            }
+            .apply {
+                with(sak.virksomhet.first()) {
+                    startDato = currentDateTime.toLocalDate()
+                    organisasjonsnummer =
+                        avgiver.organisasjonsnummer // TODO: Oslo
+                }
+            }
+            .also {
+                if (!it.sak.virksomhet.first().melding.any()) {
+                    return it
+                }
+            }
+            .apply {
+                with(sak.virksomhet.first().melding.first()) {
+                    id = java.util.UUID.randomUUID()
+                    startDato = currentDateTime.toLocalDate()
 
-            with(melding.first()) {
-                id = java.util.UUID.randomUUID()
-                startDato = currentDateTime.toLocalDate()
-
-                melder = mutableListOf(
-                    MelderType(
-                        MelderType.getRandomCode(
-                            currentDateTime.toLocalDate()
+                    melder = mutableListOf(
+                        MelderType(
+                            MelderType.getRandomCode(
+                                currentDateTime.toLocalDate()
+                            )
                         )
                     )
-                )
 
-                saksinnhold = mutableListOf(
-                    SaksinnholdType(
-                        SaksinnholdType.getRandomCode(
-                            currentDateTime.toLocalDate()
+                    saksinnhold = mutableListOf(
+                        SaksinnholdType(
+                            SaksinnholdType.getRandomCode(
+                                currentDateTime.toLocalDate()
+                            )
                         )
                     )
-                )
+                }
             }
-        }
-        return instance
     }
 
     companion object {
