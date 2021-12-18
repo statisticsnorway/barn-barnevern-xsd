416d36d7e6642a792faba37c571a96a7f07f9463
Roar Skinderviken <roar.skinderviken@gmail.com>
2021-12-18 15:17:36 +0100

    Updated modulo11 check

--- src/main/kotlin/no/ssb/barn/util/ValidationUtils.kt
+++ src/main/kotlin/no/ssb/barn/util/ValidationUtils.kt
@@ -71,28 +71,30 @@ object ValidationUtils {
     }
 
     @JvmStatic
-    fun validateSSN(ssn: String): Boolean {
-        // Validate norwegian social security number (ssn), known as fnr or dnr
-        val regex = "^\\d{11}$".toRegex()
-        if (!regex.matches(ssn)) return false
+    fun validateSSN(cid: String): Boolean {
+        val controlDigit = cid[cid.length - 1].toString()
+        val controlSumNumbers = listOf(
+            2, 3, 4, 5, 6, 7,
+            2, 3, 4, 5, 6, 7,
+            2, 3, 4, 5, 6, 7,
+            2, 3, 4, 5, 6, 7
+        )
+        val number = cid.subSequence(0, cid.length - 1).reversed().toString()
+        var sum = 0
+        for(i in number.indices) {
+            if (!number[i].isDigit()) {
+                return false
+            }
+            sum += number[i].toString().toInt() * controlSumNumbers[i]
+        }
 
-        val s = ssn.map { it.toString().toInt() }
-        var k1 = (s[0] * 3) + (s[1] * 7) + (s[2] * 6) + s[3] + (s[4] * 8) +
-                (s[5] * 9) + (s[6] * 4) + (s[7] * 5) + (s[8] * 2)
+        val output = when (val sumResult = 11 - sum % 11) {
+            11 -> "0"
+            10 -> "-"
+            else -> sumResult.toString()
+        }
 
-        val rest1 = k1 % 11
-        if (rest1 == 1) return false
-        k1 = if (rest1 == 0) 0 else 11 - rest1
-
-        var k2 =
-            (s[0] * 5) + (s[1] * 4) + (s[2] * 3) + (s[3] * 2) + (s[4] * 7) +
-                    (s[5] * 6) + (s[6] * 5) + (s[7] * 4) + (s[8] * 3) + (k1 * 2)
-
-        val rest2 = k2 % 11
-        if (rest2 == 1) return false
-        k2 = if (rest2 == 0) 0 else 11 - rest2
-
-        return (k1 == s[9]) && (k2 == s[10])
+        return output == controlDigit
     }
 
     @JvmStatic
