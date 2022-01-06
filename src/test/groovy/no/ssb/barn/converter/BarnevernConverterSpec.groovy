dfe51b67c52f280873d57e9f35d9c0453b968435
Roar Skinderviken <roar.skinderviken@gmail.com>
2022-01-06 13:55:35 +0100

    Now including validation report in BigQuery value map

--- src/test/groovy/no/ssb/barn/converter/BarnevernConverterSpec.groovy
+++ src/test/groovy/no/ssb/barn/converter/BarnevernConverterSpec.groovy
@@ -64,12 +64,14 @@ class BarnevernConverterSpec extends Specification {
         i << (1..9)
     }
 
+    def VALIDATION_REPORT_JSON = '{ "someValue": 1, "myList": [4, 8, 15, 16, 23, 42] }'
+
     def "when unmarshalling instance to map, map is not null"() {
         given:
         def xml = getResourceAsString("test01_fil09.xml")
 
         when:
-        def map = BarnevernConverter.unmarshallXmlToMap(xml)
+        def map = BarnevernConverter.unmarshallXmlAndValidationReportToMap(xml, VALIDATION_REPORT_JSON)
 
         then:
         noExceptionThrown()
