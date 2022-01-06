2ae4536472eefeac364d3555b1843dea724dd275
Roar Skinderviken <roar.skinderviken@gmail.com>
2022-01-06 22:30:10 +0100

    Added unmarshalling to JSON

--- src/main/kotlin/no/ssb/barn/converter/BarnevernConverter.kt
+++ src/main/kotlin/no/ssb/barn/converter/BarnevernConverter.kt
@@ -35,6 +35,10 @@ object BarnevernConverter {
             }
 
     @JvmStatic
+    fun unmarshallXmlToJson(xml: String): String =
+        gson.toJson(unmarshallXml(xml))
+
+    @JvmStatic
     fun marshallInstance(barnevernType: BarnevernType): String =
         StringWriter().use {
             jaxbContext
