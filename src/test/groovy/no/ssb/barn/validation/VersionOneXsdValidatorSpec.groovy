3227ecd1d8faab23f8756b15d2189cb7da80c398
John Kasper Svergja <john.svergja@ssb.no>
2022-02-01 17:07:04 +0100

    Add validateXmlStructure to TheValidator-API
    This validator method will only validate the structure of the XML against the XSD and not take other rules into account.

---
+++ src/test/groovy/no/ssb/barn/validation/VersionOneXsdValidatorSpec.groovy
@@ -0,0 +1,60 @@
+package no.ssb.barn.validation
+
+import no.ssb.barn.converter.BarnevernConverter
+import no.ssb.barn.report.WarningLevel
+import spock.lang.Specification
+import spock.lang.Subject
+
+import static no.ssb.barn.testutil.TestDataProvider.getTestContextXmlOnly
+
+class VersionOneXsdValidatorSpec extends Specification {
+
+    @Subject
+    VersionOneXsdValidator sut
+
+    @SuppressWarnings('unused')
+    def setup() {
+        sut = new VersionOneXsdValidator()
+    }
+
+    def "when validate with valid XML receive report without entries"() {
+        given:
+        def context = getTestContextXmlOnly("test01_fil01.xml")
+
+        when:
+        def report = sut.validate(context)
+
+        then:
+        0 == report.getReportEntries().size()
+        and:
+        WarningLevel.OK == report.severity
+    }
+
+    def "when validating XML with invalid rules, receive ok"() {
+        given:
+        def context = getTestContextXmlOnly("sluttdato_etter_startdato.xml")
+
+        when:
+        def report = sut.validate(context)
+
+        then:
+        0 == report.reportEntries.size()
+        and:
+        WarningLevel.OK == report.severity
+    }
+
+    def "when validate with invalid XML receive report with one entry"() {
+        given:
+        def context = getTestContextXmlOnly("invalid.xml")
+
+        when:
+        def report = sut.validate(context)
+
+        then:
+        1 == report.reportEntries.size()
+        and:
+        report.reportEntries[0].warningLevel == WarningLevel.FATAL
+        and:
+        WarningLevel.FATAL == report.severity
+    }
+}
