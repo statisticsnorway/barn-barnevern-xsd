f0933462e1a8f2a77e093bc7e23510bd18e8b4fa
Roar Skinderviken <roar.skinderviken@gmail.com>
2022-03-04 16:22:58 +0100

    BAR-321: Restored FlyttingType

---
+++ src/test/groovy/no/ssb/barn/xsd/FlyttingTypeSpec.groovy
@@ -0,0 +1,38 @@
+package no.ssb.barn.xsd
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import java.time.LocalDate
+
+class FlyttingTypeSpec extends Specification {
+
+    def "when constructor is called without values, expect no errors"() {
+        when:
+        def sut = new FlyttingType()
+
+        then:
+        noExceptionThrown()
+        and:
+        null != sut.id
+        and:
+        null == sut.migrertId
+        and:
+        null != sut.sluttDato
+        and:
+        null != sut.sluttDato
+    }
+
+    @Unroll
+    def "getCodes receive number of expected items"() {
+        expect:
+        expectedNumberOfItems == FlyttingType.getCodes(date).size()
+
+        where:
+        date                       || expectedNumberOfItems
+        LocalDate.of(2012, 12, 31) || 0
+        LocalDate.of(2013, 1, 1)   || 8
+        LocalDate.of(2013, 6, 1)   || 8
+        LocalDate.of(2101, 1, 1)   || 0
+    }
+}
