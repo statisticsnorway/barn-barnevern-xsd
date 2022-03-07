3cb5fca9df76c7ebd03ba91248b0079cce9aa611
Roar Skinderviken <roar.skinderviken@gmail.com>
2022-03-07 17:00:55 +0100

    Added tests #3

--- src/test/groovy/no/ssb/barn/converter/LocalDateAdapterSpec.groovy
+++ src/test/groovy/no/ssb/barn/converter/LocalDateAdapterSpec.groovy
@@ -9,13 +9,18 @@ class LocalDateAdapterSpec extends Specification {
     def date = LocalDate.of(2020, 1, 31)
     def dateString = "2020-01-31"
 
-    def "Marshal"() {
-        expect:
-        date == new LocalDateAdapter().unmarshal(dateString)
-    }
-
-    def "Unmarshal"() {
+    def "marshal with valid date, expect serialized date"() {
         expect:
         dateString == new LocalDateAdapter().marshal(date)
+    }
+
+    def "marshal with null, expect null"() {
+        expect:
+        null == new LocalDateAdapter().marshal(null)
+    }
+
+    def "unmarshal with valid date string, expect deserialized date"() {
+        expect:
+        date == new LocalDateAdapter().unmarshal(dateString)
     }
 }
--- src/test/groovy/no/ssb/barn/converter/LocalDateTimeAdapterSpec.groovy
+++ src/test/groovy/no/ssb/barn/converter/LocalDateTimeAdapterSpec.groovy
@@ -11,13 +11,23 @@ class LocalDateTimeAdapterSpec extends Specification {
     def dateTime = ZonedDateTime.of(LocalDateTime.of(2020, 1, 31, 23, 59, 1), ZoneId.of("+01:00"))
     def dateTimeString = "2020-01-31T23:59:01+01:00"
 
-    def "Marshal"() {
+    def "marshal with valid datetime, expect serialized datetime"() {
         expect:
         dateTimeString == new LocalDateTimeAdapter().marshal(dateTime)
     }
 
-    def "Unmarshal"() {
+    def "marshal with null, expect null"() {
+        expect:
+        null == new LocalDateTimeAdapter().marshal(null)
+    }
+
+    def "unmarshal with valid datetime string, expect deserialized datetime"() {
         expect:
         dateTime == new LocalDateTimeAdapter().unmarshal(dateTimeString)
+    }
+
+    def "unmarshal with null, expect null"() {
+        expect:
+        null == new LocalDateTimeAdapter().unmarshal(null)
     }
 }
