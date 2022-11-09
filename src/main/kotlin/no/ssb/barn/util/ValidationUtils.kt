ba479235fee1f9daca2d92c781d447a6d7faa986
Roar Skinderviken <roar.skinderviken@gmail.com>
2022-11-09 03:23:28 +0100

    BAR-661: Added support for multiple XSDs

--- src/main/kotlin/no/ssb/barn/util/ValidationUtils.kt
+++ src/main/kotlin/no/ssb/barn/util/ValidationUtils.kt
@@ -1,5 +1,6 @@
 package no.ssb.barn.util
 
+import java.io.InputStream
 import javax.xml.XMLConstants
 import javax.xml.transform.stream.StreamSource
 import javax.xml.validation.SchemaFactory
@@ -8,11 +9,19 @@ import javax.xml.validation.Validator
 
 object ValidationUtils {
 
-    fun getSchemaValidator(): Validator = SchemaFactory
+    @JvmStatic
+    val VERSION_ONE_XSD = "Barnevern.xsd"
+
+    @JvmStatic
+    fun getSchemaValidator(): Validator = getSchemaValidator(VERSION_ONE_XSD)
+
+    @JvmStatic
+    fun getSchemaValidator(xsdResourceName: String): Validator = SchemaFactory
         .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
         .apply { setFeature("http://apache.org/xml/features/disallow-doctype-decl", true) }
-        .newSchema(StreamSource(xsdAsText.byteInputStream()))
+        .newSchema(StreamSource(getResourceAsStream(xsdResourceName)))
         .newValidator()
 
-    private val xsdAsText = this.javaClass.getResource("/Barnevern.xsd")!!.readText()
+    private fun getResourceAsStream(resourceName: String): InputStream =
+        this::class.java.getResourceAsStream("/$resourceName")!!
 }
--- src/test/kotlin/no/ssb/barn/util/ValidationUtilsTest.kt
+++ src/test/kotlin/no/ssb/barn/util/ValidationUtilsTest.kt
@@ -3,15 +3,28 @@ package no.ssb.barn.util
 import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.nulls.shouldNotBeNull
+import no.ssb.barn.util.ValidationUtils.VERSION_ONE_XSD
 import no.ssb.barn.util.ValidationUtils.getSchemaValidator
 
 class ValidationUtilsTest : BehaviorSpec({
 
-    given("getSchemaValidator") {
+    given("existing XSD") {
+
         `when`("getSchemaValidator") {
 
             val schemaValidator = shouldNotThrowAny {
                 getSchemaValidator()
+            }
+
+            then("schemaValidator should not be null") {
+                schemaValidator.shouldNotBeNull()
+            }
+        }
+
+        `when`("getSchemaValidator with resource name") {
+
+            val schemaValidator = shouldNotThrowAny {
+                getSchemaValidator(VERSION_ONE_XSD)
             }
 
             then("schemaValidator should not be null") {
