68229da2ffc7c17584378014cb46bbb3bbdcfe6c
Roar Skinderviken <roar.skinderviken@gmail.com>
2022-11-24 12:04:09 +0100

    Fixed wrong test title

--- src/test/kotlin/no/ssb/barn/xsd/LovhjemmelTypeTest.kt
+++ src/test/kotlin/no/ssb/barn/xsd/LovhjemmelTypeTest.kt
@@ -13,7 +13,7 @@ import org.xml.sax.SAXException
 
 class LovhjemmelTypeTest : BehaviorSpec({
 
-    given("misc TiltakType XML") {
+    given("misc Lovhjemmel XML") {
 
         /** make sure it's possible to make a valid test XML */
         `when`("valid XML, expect no exceptions") {
