52601c868dc09dfeafc3f98b95cd44b2689dbdc3
Roar Skinderviken <roar.skinderviken@gmail.com>
2022-12-06 10:18:21 +0100

    Tests in OversendelsePrivatKravKonklusjonTypeTest are now passing

--- src/test/kotlin/no/ssb/barn/xsd/OversendelsePrivatKravKonklusjonTypeTest.kt
+++ src/test/kotlin/no/ssb/barn/xsd/OversendelsePrivatKravKonklusjonTypeTest.kt
@@ -23,7 +23,7 @@ class OversendelsePrivatKravKonklusjonTypeTest : BehaviorSpec({
             shouldNotThrowAny {
                 getSchemaValidator().validate(
                     buildKonklusjonXml(
-                        "<Konklusjon SluttDato=\"2022-11-14\" />"
+                        "<Konklusjon SluttDato=\"2022-11-14\" Kode=\"1\" Presisering=\"~Presisering~\" />"
                     ).toStreamSource()
                 )
             }
@@ -33,20 +33,54 @@ class OversendelsePrivatKravKonklusjonTypeTest : BehaviorSpec({
             /** SluttDato */
             row(
                 "missing SluttDato",
-                "<Konklusjon />",
+                "<Konklusjon Kode=\"1\" />",
                 "cvc-complex-type.4: Attribute 'SluttDato' must appear on element 'Konklusjon'."
             ),
             row(
                 "empty SluttDato",
-                "<Konklusjon SluttDato=\"\" />",
+                "<Konklusjon SluttDato=\"\" Kode=\"1\" />",
                 EMPTY_DATE_ERROR
             ),
             row(
                 "invalid SluttDato",
-                "<Konklusjon SluttDato=\"2022\" />",
+                "<Konklusjon SluttDato=\"2022\" Kode=\"1\" />",
                 INVALID_DATE_ERROR
-            )
-        ) { description, partialXml, expectedError ->
+            ),
+
+            /** Kode */
+            row(
+                "missing Kode",
+                "<Konklusjon SluttDato=\"2022-11-14\" />",
+                "cvc-complex-type.4: Attribute 'Kode' must appear on element 'Konklusjon'."
+            ),
+            row(
+                "empty Kode",
+                "<Konklusjon SluttDato=\"2022-11-14\" Kode=\"\" />",
+                "cvc-length-valid: Value '' with length = '0' is not facet-valid with respect to length '1' " +
+                        "for type '#AnonType_KodeKonklusjonOversendelsePrivatKravType'."
+            ),
+            row(
+                "invalid Kode",
+                "<Konklusjon SluttDato=\"2022-11-14\" Kode=\"4\" />",
+                "cvc-enumeration-valid: Value '4' is not facet-valid with respect to enumeration '[1, 2, 9]'. " +
+                        "It must be a value from the enumeration."
+            ),
+
+            /** Presisering */
+            row(
+                "empty Presisering",
+                "<Konklusjon SluttDato=\"2022-11-14\" Kode=\"1\" Presisering=\"\" />",
+                "cvc-minLength-valid: Value '' with length = '0' is not facet-valid with respect to " +
+                        "minLength '1' for type '#AnonType_Presisering'."
+            ),
+            row(
+                "too long Presisering",
+                "<Konklusjon SluttDato=\"2022-11-14\" Kode=\"1\" Presisering=\"${"a".repeat(301)}\" />",
+                "cvc-maxLength-valid: Value '${"a".repeat(301)}' with length = '301' is not facet-valid " +
+                        "with respect to maxLength '300' for type '#AnonType_Presisering'."
+            ),
+
+            ) { description, partialXml, expectedError ->
             `when`(description) {
                 val thrown = shouldThrow<SAXException> {
                     getSchemaValidator().validate(buildKonklusjonXml(partialXml).toStreamSource())
