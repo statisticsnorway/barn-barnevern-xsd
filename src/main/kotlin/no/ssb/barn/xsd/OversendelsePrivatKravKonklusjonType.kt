c3675a9d0a0ef1f8f7c0438e76658257319ad23d
Roar Skinderviken <roar.skinderviken@gmail.com>
2022-12-07 12:05:04 +0100

    Added Kode to OversendelsePrivatKravKonklusjonType

--- src/main/kotlin/no/ssb/barn/xsd/OversendelsePrivatKravKonklusjonType.kt
+++ src/main/kotlin/no/ssb/barn/xsd/OversendelsePrivatKravKonklusjonType.kt
@@ -1,11 +1,19 @@
 package no.ssb.barn.xsd
 
 import java.time.LocalDate
-import javax.xml.bind.annotation.*
+import javax.xml.bind.annotation.XmlAccessType
+import javax.xml.bind.annotation.XmlAccessorType
+import javax.xml.bind.annotation.XmlAttribute
+import javax.xml.bind.annotation.XmlSchemaType
+import javax.xml.bind.annotation.XmlType
 
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "OversendelsePrivatKravKonklusjonType", propOrder = ["sluttDato"])
+@XmlType(name = "OversendelsePrivatKravKonklusjonType", propOrder = ["kode","sluttDato"])
 data class OversendelsePrivatKravKonklusjonType(
+
+    @field:XmlAttribute(name = "Kode", required = true)
+    val kode: String,
+
     @field:XmlAttribute(name = "SluttDato", required = true)
     @field:XmlSchemaType(name = "date")
     val sluttDato: LocalDate
