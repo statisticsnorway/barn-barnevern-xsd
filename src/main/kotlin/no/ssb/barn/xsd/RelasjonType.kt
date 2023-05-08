d32cb04e98ad19e2af7602386d93a5a75d654103
Roar Skinderviken <roar.skinderviken@gmail.com>
2023-05-08 16:51:54 +0200

    BAR-895: RelasjonType#fraId is now of type String

--- src/main/kotlin/no/ssb/barn/xsd/RelasjonType.kt
+++ src/main/kotlin/no/ssb/barn/xsd/RelasjonType.kt
@@ -1,10 +1,10 @@
 package no.ssb.barn.xsd
 
-import java.util.UUID
 import jakarta.xml.bind.annotation.XmlAccessType
 import jakarta.xml.bind.annotation.XmlAccessorType
 import jakarta.xml.bind.annotation.XmlAttribute
 import jakarta.xml.bind.annotation.XmlType
+import java.util.UUID
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "RelasjonType", propOrder = ["id", "fraId", "fraType", "tilId", "tilType"])
@@ -12,8 +12,9 @@ data class RelasjonType(
     @field:XmlAttribute(name = "Id", required = true)
     val id: UUID,
 
+    /** fraId may contain a migrertId which is not UUID, hence type String */
     @field:XmlAttribute(name = "FraId", required = true)
-    val fraId: UUID,
+    val fraId: String,
 
     @field:XmlAttribute(name = "FraType", required = true)
     val fraType: BegrepsType,
