ac07ff1a7a94b17b01def2b37a3e347c7350c9bc
Roar Skinderviken <roar.skinderviken@gmail.com>
2024-05-23 12:31:01 +0200

    Added Gradle to 30.10.2023 version

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+description = "XSD-prosjekt for innrapportering til Barnevernsregister"
+val barnevernMaven = "artifactregistry://europe-north1-maven.pkg.dev/artifact-registry-5n/barnevern-maven"
+
+repositories { mavenCentral() }
+
+plugins {
+    kotlin("jvm") version libs.versions.kotlin.get()
+    `maven-publish`
+    id("com.google.cloud.artifactregistry.gradle-plugin") version "2.2.1"
+    id("org.sonarqube") version "5.0.0.4638"
+    jacoco
+}
+
+kotlin { jvmToolchain(21) }
+
+dependencies {
+    api(libs.jackson.module.kotlin)
+    api(libs.jackson.jakarta.rs.xml.provider)
+    api(libs.jackson.datatype.jsr310)
+
+    testImplementation(libs.kotest.runner.junit5.jvm)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "no.ssb.barn"
+            artifactId = rootProject.name
+            version = rootProject.version.toString()
+            from(components["java"])
+        }
+    }
+    repositories { maven(barnevernMaven) }
+}
+
+tasks.withType<Test> { useJUnitPlatform() }
+
+tasks.withType<JacocoReport> {
+    dependsOn(tasks.withType<Test>())
+    reports { xml.required = true }
+}
+
+sonarqube {
+    properties {
+        property("sonar.organization", "statisticsnorway")
+        property("sonar.projectKey", "statisticsnorway_barn-barnevern-xsd")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.exclusions", "**/xsd/*Type.kt")
+    }
+}
